543631027776711825aed5cb268651745104171f
recorder12 <byeonggeon@naver.com>
2020-10-10 17:21:37 -1000

    Updates

---
+++ precache-manifest.faa493768f22f06f7a9d10eff7a2ddde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615581fdb37ee2ceec1ed314ffb4267c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "81a9a11c390e948b79c4",
+    "url": "/movie_app/static/css/main.2fcca06f.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a9a11c390e948b79c4",
+    "url": "/movie_app/static/js/main.fc4d89fb.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
