c556023065fde9e86bc7575cd38579395278346b
recorder12 <byeonggeon@naver.com>
2020-10-11 17:29:50 -1000

    build Navigation

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div className="about__container">
+      <span>"Freedom is the freedom to say "</span>
+    </div>
+  );
+}
+
+export default About;
